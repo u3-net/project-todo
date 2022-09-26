825901d5e79870815c2eeac4488228770dbbafca
u3-net <u2204084@uel.ac.uk>
2022-09-26 03:45:27 +0000

    initial commit

---
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -0,0 +1,137 @@
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import 'source-map-support/register'
+
+import { verify, decode } from 'jsonwebtoken'
+import { createLogger } from '../../utils/logger'
+import Axios from 'axios'
+import { Jwt } from '../../auth/Jwt'
+import { JwtPayload } from '../../auth/JwtPayload'
+
+const logger = createLogger('auth')
+
+// TODO: Provide a URL that can be used to download a certificate that can be used
+// to verify JWT token signature.
+// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
+const jwksUrl = 'https://dev-2whzjn8l.us.auth0.com/.well-known/jwks.json'
+
+export const handler = async (
+  event: CustomAuthorizerEvent
+): Promise<CustomAuthorizerResult> => {
+  logger.info('Authorizing a user', event.authorizationToken)
+  try {
+    const jwtToken = await verifyToken(event.authorizationToken)
+    logger.info('User was authorized', jwtToken)
+
+    return {
+      principalId: jwtToken.sub,
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  } catch (e) {
+    logger.error('User not authorized', { error: e.message })
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Deny',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  }
+}
+
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
+  const token = getToken(authHeader)
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+
+  // TODO: Implement token verification
+  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
+  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
+
+  return verify(
+    token,
+    (await getSigningKey(jwksUrl, jwt.header.kid)).publicKey,
+    { algorithms: ['RS256'] }
+  ) as JwtPayload
+}
+
+function getToken(authHeader: string): string {
+  if (!authHeader) throw new Error('No authentication header')
+
+  if (!authHeader.toLowerCase().startsWith('bearer '))
+    throw new Error('Invalid authentication header')
+
+  const split = authHeader.split(' ')
+  const token = split[1]
+
+  return token
+}
+
+function certToPEM(cert) {
+  cert = cert.match(/.{1,64}/g).join('\n')
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`
+  return cert
+}
+
+const getSigningKey = async (jwksUrlm, kid) => {
+  const res = await Axios.get(jwksUrlm, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': "'true'"
+    }
+  })
+
+  const keys = res.data.keys
+
+  if (!keys || !keys.length) {
+    const errorStr = 'The JWKS endpoint did not contain any keys'
+    logger.error(errorStr)
+    throw new Error(errorStr)
+  }
+
+  const signingKeys = keys
+    .filter(
+      (key) =>
+        key.use === 'sig' && // JWK property `use` determines the JWK is for signing
+        key.kty === 'RSA' && // We are only supporting RSA
+        key.kid && // The `kid` must be present to be useful for later
+        key.x5c &&
+        key.x5c.length // Has useful public keys (we aren't using n or e)
+    )
+    .map((key) => {
+      return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) }
+    })
+
+  // If at least a single signing key doesn't exist we have a problem... Kaboom.
+  if (!signingKeys.length) {
+    const errorStr = 'The JWKS endpoint did not contain any signing keys'
+    logger.error(errorStr)
+    throw new Error(errorStr)
+  }
+
+  const signingKey = signingKeys.find((key) => key.kid === kid)
+
+  if (!signingKey) {
+    const errorStr = `Unable to find a signing key that matches '${kid}'`
+    logger.error(errorStr)
+    throw new Error(errorStr)
+  }
+
+  logger.info(`Able to find a signing key that matches '${kid}'`, signingKey)
+  return signingKey
+}
---
+++ backend/src/lambda/http/getTodos.ts
@@ -0,0 +1,42 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+
+import { getTodosForUser as getTodosForUser } from '../../helpers/todos'
+import { getUserId } from '../utils'
+
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('getTodo')
+
+// TODO: Get all TODO items for a current user
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // Write your code here
+    const userId = getUserId(event)
+
+    logger.info('Event info: ', event)
+
+    const todos = await getTodosForUser(userId)
+
+    logger.info('Get todos result: ' + JSON.stringify(todos))
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        items: todos
+      })
+    }
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
