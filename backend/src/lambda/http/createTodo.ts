da866975733892acc1e7cf3b9bc10f79e856f911
u3-net <u2204084@uel.ac.uk>
2022-09-28 01:10:31 +0000

    new

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -4,7 +4,7 @@ import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils'
-import { createTodo } from '../../helpers/todos'
+import { createTodo } from '../../businessLayer/todos'
 import { createLogger } from '../../utils/logger'
 
 const logger = createLogger('createTodo')
--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -4,7 +4,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { deleteTodo } from '../../helpers/todos'
+import { deleteTodo } from '../../businessLayer/todos'
 import { getUserId } from '../utils'
 
 import { createLogger } from '../../utils/logger'
